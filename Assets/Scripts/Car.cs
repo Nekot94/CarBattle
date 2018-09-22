8318a178bd08ea59f70b7c0d98b77dacd34f6f47
Nekot94 <nekonato9@gmail.com>
2018-09-22 14:00:30 +0300

    build

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,20 +4,44 @@ public class Car : MonoBehaviour
 {
     public float speed = 10;
     public int playerNumber = 1;
+    public AudioSource crash;
+    public AudioSource motor;
+    public float jumpSpeed = 30;
+    public float rotationSpeed = 100;
 
-	void Start ()
-    {
-		
-	}
 
 	void Update ()
     {
         float move = Input.GetAxis("Horizontal" + playerNumber) * speed * Time.deltaTime;
+        float rotate = Input.GetAxis("Vertical" + playerNumber) * rotationSpeed * Time.deltaTime;
         if (playerNumber == 2)
         {
             move = -move;
         }
-        transform.Translate(0, 0, move);
+        float moveY = 0;
+        if (Input.GetButtonDown("Jump" + playerNumber))
+        {
+            GetComponent<Rigidbody>().AddForce(0,jumpSpeed,0);
+        }
+        transform.Translate(0, moveY, move);
+        transform.Rotate(-rotate, 0, 0);
+
+        if (move != 0 && !motor.isPlaying)
+        {
+            motor.Play();
+        }
+        if (move == 0)
+        {
+            motor.Stop();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && !crash.isPlaying)
+        {
+            crash.Play();
+        }
     }
 
 
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,24 +8,52 @@ public class GameController : MonoBehaviour
 {
     public Text winText;
     public static GameController instance;
-    public bool isWin; 
+    public bool isWin;
+    static int score1, score2;
+    public Text score1Text, score2Text;
+    public int maxScore = 10;
+    AudioSource source;
+    public AudioClip uwin;
 
-	void Start ()
+    void Start ()
     {
+        source = GetComponent<AudioSource>();
         instance = this;
         winText.text = "";
+        score1Text.text = "Игрок 1: " + score1;
+        score2Text.text = "Игрок 2: " + score2;
 
-	}
+    }
 	
     public void Win(int playerNumber)
     {
         if (playerNumber == 1)
+        {
             winText.color = Color.red;
+            score1++;
+        }
         else
+        {
             winText.color = Color.blue;
-        winText.text = "Игрок " + playerNumber + " победил";
+            score2++;
+        }
         StartCoroutine(Restart());
         isWin = true;
+        if (score1 == maxScore || score2 == maxScore)
+        {
+             winText.text = "Игрок " + playerNumber + " чемпион";
+            score1 = 0;
+            score2 = 0;
+            source.clip = uwin;
+            source.Play();
+
+        }
+        else
+        {
+            winText.text = "Игрок " + playerNumber + " победил";
+            source.Play();
+        }
+
     }
 
     IEnumerator Restart()
